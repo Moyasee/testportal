8bd0993c2c96cf83abb599d2a62e0942b19838b5
zxcsix-zxc <terranoiz@icloud.com>
2025-02-06 13:36:46 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',  // This is optional but recommended for better performance
+  images: {
+    unoptimized: true   // If you're using next/image
+  },
+  // Add any other necessary configurations
+}
 
 module.exports = nextConfig 
